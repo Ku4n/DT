63851abc36a05362e1a2281845b39b55879cbf8b
ku4n <410078329@qq.com>
2018-03-19 01:37:34 +0800

    增加判断

--- App/Home/Model/LunchModel.class.php
+++ App/Home/Model/LunchModel.class.php
@@ -68,63 +68,8 @@ class LunchModel extends Model
     }
 
 
-    public function test($userName, $userId, $now, $time)
+    public function Del($userName, $userId, $now, $time)// userName 报名人姓名 , userId 报名人ID , now 当前时间戳 , time date格式时间
     {
-        $map = D('signup');
-
-        $data = array(
-            'uesr' => $userName,
-            'userId' => $userId,
-            'sign_time' => $now,
-            'time' => $time,
-        );
-
-        $add = $map -> add($data);
-
-        if($add){
-
-            $db = D('user');
-
-            $update = $db -> where(['userId' => $userId]) -> find();
-
-            if($update['username'] == $userName){
-                $update['status'] = 1;
-
-                $change = $db -> save($update);
-
-                if($change){
-
-                    $record = M('log_record');
-
-                    $operate = 1;
-
-                    $date = array(
-                        'user' => $userName,
-                        'userId' => $userId,
-                        'operate' => $operate,
-                        'update_time' => $now,
-                        'time' => $time
-                    );
-
-                    $save = $record -> create($date);
-
-                    if($save){
-                        return true;
-                    }else{
-                        return 3;
-                    }
-
-                }else{
-                    return 2;
-                }
-
-            }else{
-                return 1;
-            }
-
-        }else{
-            return 0;
-        }
 
 
     }
